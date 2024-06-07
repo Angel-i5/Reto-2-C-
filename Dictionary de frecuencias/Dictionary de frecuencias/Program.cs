a39a7a81fe44a426da02bf51c68a756e6c5ebb24
angel flores <af5300439@gmail.com>
2024-06-07 16:49:42 -0400

    contador de palabras

---
+++ Dictionary de frecuencias/Dictionary de frecuencias/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("introduce un texto o parrafo");
+string texto = Console.ReadLine();
+
+string[] parrafo = texto.Split(new char[] { ' ' });
+
+Dictionary<string,int> frecuencia = new Dictionary<string,int>();
+
+foreach (string palabra in parrafo) 
+{
+    
+    if (frecuencia.ContainsKey(palabra))
+    {
+        frecuencia[palabra]++;
+    }
+    else
+    {
+        frecuencia[palabra] = 1;
+    }
+}
+
+foreach (var kvp in frecuencia)
+{
+    Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+}
+
+
