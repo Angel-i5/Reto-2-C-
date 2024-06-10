803c1993f64360997280e7ad10729eebd8a191cf
angel flores <af5300439@gmail.com>
2024-06-09 21:55:09 -0400

    loteria de numeros

---
+++ loteria de numeros/loteria de numeros/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("introduce 6 numeros para la loteria entre 1 y 49");
+
+
+Random random = new Random();
+HashSet<int> numerosAleatorios = new HashSet<int>();
+
+while (numerosAleatorios.Count <= 6)
+{
+    int numeroAleatorio = random.Next(1, 49);
+    if (!numerosAleatorios.Contains(numeroAleatorio))
+    {
+        numerosAleatorios.Add(numeroAleatorio);
+    }
+    
+};
+
+ 
+
+HashSet<int> numerosUsuario = new HashSet<int>(6);
+
+for (int i = 0; i < 6; i++)
+{
+
+    Console.WriteLine("introduce el numero");
+    int numero = int.Parse(Console.ReadLine());
+
+    if (numerosUsuario.Contains(numero))
+    {
+        numerosUsuario.Add(numero);
+    }
+
+}
+
+int aciertos = 0;
+
+foreach (int resultados in numerosUsuario)
+{
+    if (numerosUsuario.Contains(resultados))
+    {
+        aciertos++;
+    }
+}
+  Console.WriteLine("Número de aciertos: " + aciertos);
+
+  Console.WriteLine("los numeros ganadores son");
+foreach (int numerosGanadores in numerosAleatorios)
+{
+  
+ Console.WriteLine($"{numerosGanadores}");
+};
+
